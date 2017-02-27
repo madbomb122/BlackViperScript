43e3cee6744a67f3acd9656924650537626e9e19
madbomb122 <madbomb122@users.noreply.github.com>
2017-02-26 16:37:24 -0800

    Update _Win10-BlackViper.bat

--- _Win10-BlackViper.bat
+++ _Win10-BlackViper.bat
@@ -1,2 +1,18 @@
 @ECHO OFF
-PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %~dp0BlackViper-Win10.ps1' -Verb RunAs}"
+
+:: Remove the 2 Colons infront of the command you want to run
+
+:: First Line Description of Command
+:: Second Line Command
+
+::Run script with Menu Selection
+::PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %~dp0BlackViper-Win10.ps1' -Verb RunAs}"
+
+::Run script with Black Viper Default
+::PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %~dp0BlackViper-Win10.ps1 -Set 1' -Verb RunAs}"
+
+::Run script with Black Viper Safe
+::PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %~dp0BlackViper-Win10.ps1 -Set 2' -Verb RunAs}"
+
+::Run script with Black Viper Tweaked
+::PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %~dp0BlackViper-Win10.ps1 -Set 3' -Verb RunAs}"
