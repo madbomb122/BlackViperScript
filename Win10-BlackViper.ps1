95409ff57c2869a233f953232663e64f155b0d33
madbomb122 <madbomb122@users.noreply.github.com>
2017-03-13 09:46:12 -0700

    Update Win10-BlackViper.ps1

--- Win10-BlackViper.ps1
+++ Win10-BlackViper.ps1
@@ -1,12 +1,12 @@
 ##########
-# Win10 Black Viper Service Settings
+# Win10 Black Viper Service Configuration Script
 # 
-# Black Viper's Settings from
+# Black Viper's Service Configurations From
 # Website: http://www.blackviper.com/service-configurations/black-vipers-windows-10-service-configurations/
 #
 # Script + Menu By
 # Author: Madbomb122
-# Website: https://github.com/madbomb122/BlackViperScript/
+# Website: https://github.com/madbomb122/
 # Version: 1.3, 03-13-2017
 #
 # Release Type: Stable
@@ -181,10 +181,14 @@ $Script:Automated = $false
 # Back Viper's Website
 # http://www.blackviper.com/service-configurations/black-vipers-windows-10-service-configurations/
 
-
 $WinEdition = gwmi win32_operatingsystem | % caption
 #Pro = Microsoft Windows 10 Pro
 #Home = Microsoft Windows 10 Home 
+
+$BuildVer = [environment]::OSVersion.Version.build
+# 14393 = anniversary update
+# 10586 = first major update
+# 10240 = first release
 
 $ServicesList = @(
  #(Service Name, Def-Home, Def-Pro , Safe, Tweaked)
@@ -356,12 +360,12 @@ If($WinEdition -eq "Microsoft Windows 10 Home" -or $WinEdition -eq "Microsoft Wi
         } ElseIf($SettingImp.ToLower() -eq "tweaked"){
             Black_Viper_Set 3
         } Else{
-            Write-Host "Invalid Selection"  -ForegroundColor Blue -BackgroundColor Black
-            Write-Host ""            
-            Write-Host "Valid Selections are:"  -ForegroundColor Blue -BackgroundColor Black
-            Write-Host "1 or Default"  -ForegroundColor Green -BackgroundColor Black
-            Write-Host "2 or Safe"  -ForegroundColor Yellow -BackgroundColor Black
-            Write-Host "3 or Tweaked"  -ForegroundColor Red -BackgroundColor Black
+            Write-Host "Invalid Selection" -ForegroundColor Blue -BackgroundColor Black
+            Write-Host ""
+            Write-Host "Valid Selections are:" -ForegroundColor Blue -BackgroundColor Black
+            Write-Host "1 or Default" -ForegroundColor Green -BackgroundColor Black
+            Write-Host "2 or Safe" -ForegroundColor Yellow -BackgroundColor Black
+            Write-Host "3 or Tweaked" -ForegroundColor Red -BackgroundColor Black
             Write-Host ""
             Write-Host "Press Any key to Close..." -ForegroundColor White -BackgroundColor Black
             $key = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown,AllowCtrlC")
@@ -371,10 +375,10 @@ If($WinEdition -eq "Microsoft Windows 10 Home" -or $WinEdition -eq "Microsoft Wi
     }
 } Else {
     Write-Host "Websites:"
-    Write-Host "https://github.com/madbomb122/Win10Script/"
+    Write-Host "https://github.com/madbomb122/"
     Write-Host "http://www.blackviper.com/"
     Write-Host ""
-    Write-Host "Not a Valid OS for this Script."  -ForegroundColor Red -BackgroundColor Black
+    Write-Host "Not a Valid OS for this Script." -ForegroundColor Red -BackgroundColor Black
     Write-Host "Win 10 Home and Pro Only"
     Write-Host ""
     Write-Host "Press Any key to Close..." -ForegroundColor White -BackgroundColor Black
