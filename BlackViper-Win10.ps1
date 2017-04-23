415fafa074f80da34dbb584887734d05951ed8a8
madbomb122 <madbomb122@users.noreply.github.com>
2017-04-23 08:37:30 -0700

    Update BlackViper-Win10.ps1

--- BlackViper-Win10.ps1
+++ BlackViper-Win10.ps1
@@ -25,21 +25,29 @@ $Release_Type = "Stable"
 ## !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 <#--------------------------------------------------------------------------------
-    Copyright (c) 2017 Madbomb122 - Black Viper Service Script
     Copyright (c) 1999-2017 Charles "Black Viper" Sparks - Services Configuration
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    The MIT License (MIT)
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    Copyright (c) 2017 Madbomb122 - Black Viper Service Script
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 --------------------------------------------------------------------------------#>
 
 <#--------------------------------------------------------------------------------
@@ -400,33 +408,41 @@ Function CopyrightDisplay {
 
 $CopyrightItems = @(
 '                    Copyright                    ',
-'                                                 ',
 ' Services Configuration                          ',
 ' Copyright (c) 1999-2017                         ',
 ' Charles "Black Viper" Sparks                    ',
 '                                                 ',
-' This Script                                     ',
+'              The MIT License (MIT)              ',
+'                                                 ',
+' Black Viper Service Script (This Script)        ',
 ' Copyright (c) 2017 Madbomb122                   ',
 '                                                 ',
-' This program is free software: you can          ',
-' redistribute it and/or modify This program is   ',
-' free software This program is free software:    ',
-' you can redistribute it and/or modify it under  ',
-' the terms of the GNU General Public License as  ',
-' published by the Free Software Foundation,      ',
-' version 3 of the License.                       ',
+' Permission is hereby granted, free of charge, to',
+' any person obtaining a copy of this software and',
+' associated documentation files (the "Software"),',
+' to deal in the Software without restriction,    ',
+' including without limitation the rights to use, ',
+' copy, modify, merge, publish, distribute,       ',
+' sublicense, and/or sell copies of the Software, ',
+' and to permit persons to whom the Software is   ',
+' furnished to do so, subject to the following    ',
+' conditions:                                     ',
 '                                                 ',
-' This program is distributed in the hope that it ',
-' will be useful, but WITHOUT ANY WARRANTY;       ',
-' without even the implied warranty of            ',
-' MERCHANTABILITY or FITNESS FOR A PARTICULAR     ',
-' PURPOSE.  See the GNU General Public License    ',
-' for more details.                               ',
+' The above copyright notice and this permission  ',
+' notice shall be included in all copies or       ',
+' substantial portions of the Software.           ',
 '                                                 ',
-' You should have received a copy of the GNU      ',
-' General Public License along with this program. ',
-' If not, see <http://www.gnu.org/licenses/>.     ',
-'                                                 ')
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT       ',
+' WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,       ',
+' INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF  ',
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR       ',
+' PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL  ',
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR  ',
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER  ',
+' IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,    ',
+' ARISING FROM, OUT OF OR IN CONNECTION WITH THE  ',
+' SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    ',
+' SOFTWARE.                                       ')
 
 $ServicesTypeList = @(
     '',          #0 -None (Not Installed, Default Only)
