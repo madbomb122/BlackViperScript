a8e520f92b60f9483774f7f78011a757203810f8
madbomb122 <madbomb122@gmail.com>
2017-05-06 07:40:59 -0700

    Update _Win10-BlackViper.bat

--- _Win10-BlackViper.bat
+++ _Win10-BlackViper.bat
@@ -1,48 +1,70 @@
 @ECHO OFF
-
 :: Instructions
 :: Bat, Script MUST be in same Folder
-:: Change RunOption to = what you want to run (just the number)
+:: Change Option to = one of the listed options (mostly yes or no)
 
-Set Run_Option=0
-:: Anything other than following does nothing
+Set Black_Viper=0
 :: 0 = Run with Menu
 :: 1 = Run with Windows Default Service Configuration
 :: 2 = Run with Black Viper Safe
 :: 3 = Run with Black Viper Tweaked
-:: 4 = Exit (Dont Run script)
 
-Set Script_File=BlackViper-Win10.ps1
+:: Change these to yes or no
+Set Accept_ToS=no
+:: no = See ToS
+:: yes = Skip ToS (You accepted it)
 
+Set Automated=no 
+:: no = Pause on - User input, On Errors, or End of Script
+:: yes = Close on - User input, On Errors, or End of Script
+:: yes, Implies that you accept the "ToS"
+								
+:: Update Checks   
+:: If update is found it will Auto-download and use that (with your settings)       
+Set Script=no
+Set Service=no
+Set Internet_Check=yes 
+:: Internet_Check only matters If Script or Service is yes and pings to github.com is blocked
+
+:: Skip Script Check
+:: SKIP AT YOUR OWN RISK
+Set Skip_Build_Check=no
+Set Skip_Edition_Check=no
+
+::----------------------------------------------------------------------
 :: Do not change unless you know what you are doing
+Set Script_File=BlackViper-Win10.ps1
 Set Script_Directory=%~dp0
 Set Script_Path=%Script_Directory%%Script_File%
 
 :: DO NOT CHANGE ANYTHING PAST THIS LINE
 ::----------------------------------------------------------------------
-Set Use_Arg=yes
-
-If /i %Run_Option%==0 Set Use_Arg=no
-If /i %Run_Option%==4 Exit
-If /i %Run_Option%==5 Set Use_Arg=diag
-
 SETLOCAL ENABLEDELAYEDEXPANSION
-If /i %Use_Arg%==no (
-    echo "Running !Script_File!, with Menu option"
-    powershell.exe -noprofile -ExecutionPolicy Bypass -command "&{start-process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -file \"!Script_Path!\"' -verb RunAs}"
-)
-ENDLOCAL DISABLEDELAYEDEXPANSION
+If %Accept_ToS%==yes Set Run_Option=!Run_Option!-atos
 
-SETLOCAL ENABLEDELAYEDEXPANSION
-If /i %Use_Arg%==yes (
-    echo "Running !Script_File! -Set !Run_Option!"
-    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"!Script_Path!\" -Set \"!Run_Option!\"' -Verb RunAs}";
-)
-ENDLOCAL DISABLEDELAYEDEXPANSION
+If %Black_Viper%==1 Set Run_Option=!Run_Option!-default
+If %Black_Viper%==default Set Run_Option=!Run_Option!-default
 
-SETLOCAL ENABLEDELAYEDEXPANSION
-If /i %Use_Arg%==diag (
-    echo "Running !Script_File! to Show Diagnostic Output"
-    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"!Script_Path!\" -Set diag' -Verb RunAs}";
-)
+If %Black_Viper%==2 Set Run_Option=!Run_Option!-safe
+If %Black_Viper%==safe Set Run_Option=!Run_Option!-safe
+
+If %Black_Viper%==3 Set Run_Option=!Run_Option!-tweaked
+If %Black_Viper%==tweaked Set Run_Option=!Run_Option!-tweaked
+
+If %Skip_Build_Check%==yes Set Run_Option=!Run_Option!-sbc
+
+If %Skip_Edition_Check%==yes Set Run_Option=!Run_Option!-sec
+
+If %Internet_Check%==no Set Run_Option=!Run_Option!-sic
+
+If %Script%==yes Set Run_Option=!Run_Option!-usc
+
+If %Service%==yes Set Run_Option=!Run_Option!-use
+
+If %Automated%==yes Set Run_Option=!Run_Option!-auto
+
+If %Diag%==yes Set Run_Option=!Run_Option!-diag
+
+echo "Running !Script_File!"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "!Script_Path! !Run_Option!"' -Verb RunAs}";
 ENDLOCAL DISABLEDELAYEDEXPANSION
