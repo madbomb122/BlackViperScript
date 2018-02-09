c33037be258066b0e07d9399c4c048877302ef5c
madbomb122 <madbomb122@gmail.com>
2018-02-08 17:59:38 -0800

    Update _Win10-BlackViper.bat

--- _Win10-BlackViper.bat
+++ _Win10-BlackViper.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
-:: Version 1.1
-:: December 14th, 2017
+:: Version 1.2
+:: February 7th, 2017
 
 :: Instructions
 :: Bat, Script MUST be in same Folder
@@ -16,6 +16,10 @@ Set All_or_Min=Min
 :: all = Changes all windows services
 :: min = Changes just the services that are different from default to tweaked/safe
 
+Set Skip_XBox_Services=no
+:: no = change xbox services
+:: yes = skip xbox services
+
 :: Change these to yes or no
 Set Accept_ToS=no
 :: no = See ToS
@@ -29,7 +33,12 @@ Set Automated=no
 Set Backup_Current_Service_Conf=no
 :: no = Dont backup Your Current Service Configuration before services are changes
 :: yes = Backup Your Current Service Configuration before services are changes
+Set Backup_Current_Service_Type=csv
+:: csv = Save service backup as csv file
+:: reg = Save service backup as reg file
+:: both = Save service backup as csv and reg file
 :: Filename will be "(ComputerName)-Service-Backup.csv"
+:: Filename will be "(ComputerName)-Service-Backup.reg"
 
 Set Load_Custom_Service_Config=no
 :: no = Use the default Black Viper Service Configuration
@@ -38,7 +47,7 @@ Set Service_Config_File=Computername-Service-Backup.csv
 :: Set filename if being used otherwise ignore
 
 :: Update Checks   
-:: If update is found it will Auto-download and use that (with your settings)       
+:: If update is found it will Auto-download and use that (with your settings)	   
 Set Script=no
 Set Service=no
 Set Internet_Check=yes 
@@ -75,30 +84,45 @@ Set Script_Path=%Script_Directory%%Script_File%
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 If /i not "%*"=="" (
-    for %%i in (%*) do (
-        If /i !LoadServiceArg!==yes (
-            Set Service_Config_File=%%i
-            Set LoadServiceArg=no
-        )
-        If /i %%i==-lcsc Set LoadServiceArg=yes
+	for %%i in (%*) do (
+		If /i !LoadServiceArg!==yes (
+			Set Service_Config_File=%%i
+			Set LoadServiceArg=no
+		)
+		If /i %%i==-lcsc Set LoadServiceArg=yes
 
-        If /i %%i==-bcsc Set Backup_Current_Service_Conf=yes
-        If /i %%i==-atos Set Accept_ToS=yes
-        If /i %%i==-auto Set Automated=yes
-        If /i %%i==-usc Set Script=yes
-        If /i %%i==-use Set Service=yes
-        If /i %%i==-sic Set Internet_Check=yes
-        If /i %%i==-sbc Set Skip_Build_Check=yes
-        If /i %%i==-sec Set Skip_Edition_Check=Pro
-        If /i %%i==-secp Set Skip_Edition_Check=Pro
-        If /i %%i==-sech Set Skip_Edition_Check=Home
-        If /i %%i==-default Set Black_Viper=1
-        If /i %%i==-safe Set Black_Viper=2
-        If /i %%i==-tweaked Set Black_Viper=3
-        If /i %%i==-diag Set Diagnostic=yes
-        If /i %%i==-log Set Log=Yes
-        If /i %%i==-baf Set Log_Before_After=yes
-    )
+		If /i %%i==-bscc (
+			Set Backup_Current_Service_Conf=yes
+			Set Backup_Current_Service_Type=csv
+		)
+
+		If /i %%i==-bscr (
+			Set Backup_Current_Service_Conf=yes
+			Set Backup_Current_Service_Type=reg
+		)
+
+		If /i %%i==-bscb (
+			Set Backup_Current_Service_Conf=yes
+			Set Backup_Current_Service_Type=both
+		)
+
+		If /i %%i==-sxb Set Skip_XBox_Services=yes
+		If /i %%i==-atos Set Accept_ToS=yes
+		If /i %%i==-auto Set Automated=yes
+		If /i %%i==-usc Set Script=yes
+		If /i %%i==-use Set Service=yes
+		If /i %%i==-sic Set Internet_Check=yes
+		If /i %%i==-sbc Set Skip_Build_Check=yes
+		If /i %%i==-sec Set Skip_Edition_Check=Pro
+		If /i %%i==-secp Set Skip_Edition_Check=Pro
+		If /i %%i==-sech Set Skip_Edition_Check=Home
+		If /i %%i==-default Set Black_Viper=1
+		If /i %%i==-safe Set Black_Viper=2
+		If /i %%i==-tweaked Set Black_Viper=3
+		If /i %%i==-diag Set Diagnostic=yes
+		If /i %%i==-log Set Log=Yes
+		If /i %%i==-baf Set Log_Before_After=yes
+	)
 )
 
 If /i %Accept_ToS%==yes Set Run_Option=!Run_Option! -atos
@@ -115,7 +139,13 @@ If /i %Black_Viper%==tweaked Set Run_Option=!Run_Option! -tweaked
 If /i %All_or_Min%==All Set Run_Option=!Run_Option! -all
 If /i %All_or_Min%==Min Set Run_Option=!Run_Option! -min
 
-If /i %Backup_Current_Service_Conf%==yes Set Run_Option=!Run_Option! -bcsc
+If /i %Skip_XBox_Services%==yes Set Run_Option=!Run_Option! -sxb
+
+If /i %Backup_Current_Service_Conf%==yes (
+	If /i %Backup_Current_Service_Type%==csv Set Run_Option=!Run_Option! -bscc
+	If /i %Backup_Current_Service_Type%==reg Set Run_Option=!Run_Option! -bscr
+	If /i %Backup_Current_Service_Type%==both Set Run_Option=!Run_Option! -bscb
+)
 
 If /i %Load_Custom_Service_Config%==yes Set Run_Option=!Run_Option! -lcsc %Service_Config_File%
 
